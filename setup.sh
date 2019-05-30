15aa709204cc1b4548f79d5a88a3e463f1344376
rektosauruz <kymzbrk@gmail.com>
2019-05-30 10:21:51 +0300

    yess

--- setup.sh
+++ setup.sh
@@ -19,24 +19,29 @@ RESET1=$ESC"0m"
 
 
 ##check for root
-if [[ "${EUID}" -ne 0 ]]; then
-  echo -e "${GREEN}run${RESET} ${RED}setup.sh${RESET} ${GREEN}with${RESET} ${RED}root !${RESET}"
-  exit 1
-fi
 
 
-#### CREATING REQUIRED FOLDERS
+if [ "`whoami`" != "root" ]; then 
+	echo "Please run as root"
 
-sudo mkdir data_cache
-sudo mkdir node_log
+else 
+
+	sudo mkdir data_cache
+	sudo mkdir node_log
 
 
 #### CREATING REQUIRED FILES
-sudo touch streaming.log
+	sudo touch streaming.log
 
 
 #### SETTING R-W-E
-sudo chmod 777 data_cache/
-sudo chmod 777 node_log/
-sudo chmod 777 streaming.log
+	sudo chmod 777 data_cache/
+	sudo chmod 777 node_log/
+	sudo chmod 777 streaming.log
+
+
+fi
+
+
+
 
